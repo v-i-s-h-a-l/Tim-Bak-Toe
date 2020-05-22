63df908fe50e07e3bd0be48b7ed3a1762cd43ae9
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-05-22 10:06:27 +0530

    add gitignore

---
+++ Tim-Bak-Toe-Mac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Tim-Bak-Toe-Mac
+//
+//  Created by Vishal Singh on 22/05/20.
+//  Copyright © 2020 v-i-s-h-a-l. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

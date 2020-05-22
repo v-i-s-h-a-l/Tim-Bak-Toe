daca9e29fb7d586d9cb0aa007d6d4d3c063c040b
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-05-22 10:07:49 +0530

    add macOS target

--- Tim-Bak-Toe/Views/ContentView.swift
+++ Tim-Bak-Toe/Views/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ForEach(["iPhone SE", "iPhone XS Max", "iPad Pro"], id: \.self) { deviceName in
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
     }
 }

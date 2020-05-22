84d9ea542ea05939906911635ab9d70c530132f3
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-05-22 10:26:18 +0530

    create transition from contenview to game view

--- Tim-Bak-Toe/Views/ContentView.swift
+++ Tim-Bak-Toe/Views/ContentView.swift
@@ -9,8 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            NavigationLink(destination: GameView(), label: { Text("Play").font(.largeTitle) })
+        }
     }
 }
 

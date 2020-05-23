fd8bb86dc96bf231ef8007b53376c1aaa95f043f
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-05-23 11:40:07 +0530

    create theme enum

---
+++ Tim-Bak-Toe/Helpers/Theme.swift
@@ -0,0 +1,28 @@
+//
+//  Theme.swift
+//  Tim-Bak-Toe
+//
+//  Created by Vishal Singh on 23/05/20.
+//  Copyright © 2020 v-i-s-h-a-l. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    enum Col {
+        static var piece = Color.red
+
+        static var gameBackground = Color.gray.opacity(0.7)
+        static var boardCell = Color.secondary
+
+        enum Shadow {
+            enum BoardCell {
+                static var none = Color.clear
+                static var occupied = Color.red
+                static var origin = Color.blue
+                static var welcome = Color.yellow
+            }
+        }
+    }
+}

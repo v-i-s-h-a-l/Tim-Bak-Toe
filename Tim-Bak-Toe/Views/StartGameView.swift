4b693febf9f81be64165254d8cc929e69d90c990
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-05-27 06:34:00 +0530

    extract board view and start game view

---
+++ Tim-Bak-Toe/Views/StartGameView.swift
@@ -0,0 +1,31 @@
+//
+//  StartGameView.swift
+//  Tim-Bak-Toe
+//
+//  Created by Vishal Singh on 26/05/20.
+//  Copyright © 2020 v-i-s-h-a-l. All rights reserved.
+//
+
+import SwiftUI
+
+struct StartGameView: View {
+
+    @Binding var showGameScreen: Bool
+    private let linearGradient = LinearGradient(gradient: Gradient(colors: [Color.blue, Color.red]), startPoint: .top, endPoint: .bottom)
+
+    var body: some View {
+        Button(action: {
+            self.showGameScreen.toggle()
+        }) {
+            Text("P L A Y")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(Color.white)
+                .multilineTextAlignment(.center)
+                .padding()
+        }
+        .background(linearGradient)
+        .border(Color.accentColor, width: 4)
+        .cornerRadius(10.0)
+    }
+}

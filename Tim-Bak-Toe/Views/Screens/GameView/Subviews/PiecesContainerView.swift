879b19ac7ce5a78e04b4e93de9e3530fa892c242
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-06-05 04:58:31 +0530

    update piece container dimensions

--- Tim-Bak-Toe/Views/Screens/GameView/Subviews/PiecesContainerView.swift
+++ Tim-Bak-Toe/Views/Screens/GameView/Subviews/PiecesContainerView.swift
@@ -14,8 +14,10 @@ struct PiecesContainerView: View {
     @EnvironmentObject var viewModel: GameViewModel
     
     var body: some View {
-        VStack {
-            HStack(spacing: 5) {
+        let spacingForPieces = pieceSize.height / 11.0
+        let timerHeight = pieceSize.height / 8.0
+        return VStack {
+            HStack(spacing: spacingForPieces) {
                 ForEach(viewModel.peerPieces) {
                     PieceView(viewModel: $0, size: self.pieceSize)
                         .padding([.trailing])
@@ -26,16 +28,16 @@ struct PiecesContainerView: View {
             .zIndex(ZIndex.playerPiecePlaced)
 
             TimerView(viewModel: viewModel.peerTimerViewModel, isRightEdged: true)
-                .frame(height: 10)
+                .frame(height: timerHeight)
                 .padding([.leading, .trailing])
             
             Spacer()
             
             TimerView(viewModel: viewModel.hostTimerViewModel, isRightEdged: false)
-                .frame(height: 10)
+                .frame(height: timerHeight)
                 .padding([.leading, .trailing])
             
-            HStack(spacing: 5) {
+            HStack(spacing: spacingForPieces) {
                 ForEach(viewModel.hostPieces) {
                     PieceView(viewModel: $0, size: self.pieceSize)
                         .padding([.leading])
@@ -50,6 +52,6 @@ struct PiecesContainerView: View {
 
 struct PiecesContainerView_Previews: PreviewProvider {
     static var previews: some View {
-        PiecesContainerView(pieceSize: CGSize(width: 50, height: 50)).environmentObject(GameViewModel())
+        PiecesContainerView(pieceSize: CGSize(width: 90, height: 90)).environmentObject(GameViewModel())
     }
 }

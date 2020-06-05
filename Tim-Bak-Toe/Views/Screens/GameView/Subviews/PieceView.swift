31bb780325c8e72b1bb5097112593352f33d8de2
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-06-05 06:56:22 +0530

    refactor PieceView: move helpers down

--- Tim-Bak-Toe/Views/Screens/GameView/Subviews/PieceView.swift
+++ Tim-Bak-Toe/Views/Screens/GameView/Subviews/PieceView.swift
@@ -8,6 +8,76 @@
 
 import SwiftUI
 
+struct PieceView: View {
+    
+    @ObservedObject var viewModel: PieceViewModel
+    var size: CGSize
+
+    var body: some View {
+        let isDragged = viewModel.zIndex == ZIndex.playerPieceDragged
+        let isDisabled = viewModel.disabled
+        let shadowCastedRadius =  viewModel.stateMultiplier * (isDisabled ? 0.0 : size.height / 40.0)
+        let lightSourceShadowRadius = viewModel.stateMultiplier == 1.0 ? (isDisabled ? 0.0 : size.height / 40.0) : 0
+        let blurAmount = 1 * viewModel.stateMultiplier
+        let lineWidthForPieceSign = size.height / 9.0
+        let insetAmuontForPieceSign = size.height / 3.5
+        return ZStack {
+            Circle()
+                .fill(Theme.Col.piece)
+                .shadow(color: Theme.Col.lightSource, radius: lightSourceShadowRadius, x: -lightSourceShadowRadius, y: -lightSourceShadowRadius)
+                .shadow(color: Theme.Col.shadowCasted, radius: shadowCastedRadius, x: shadowCastedRadius, y: shadowCastedRadius)
+                .animation(Animation.default)
+                .blur(radius: blurAmount)
+            Circle()
+                .fill(Theme.Col.piece)
+            if isDragged || isDisabled {
+                Circle()
+                    .fill(LinearGradient(Theme.Col.lightSource, Theme.Col.shadowCasted))
+            }
+            if !isDisabled {
+                Circle()
+                    .stroke(LinearGradient(Theme.Col.lightSource, Theme.Col.shadowCasted), lineWidth: shadowCastedRadius / 2.0)
+                    .blur(radius: shadowCastedRadius / 2.0)
+            }
+            if viewModel.style == .O {
+                Circle()
+                    .inset(by: insetAmuontForPieceSign)
+                    .stroke(viewModel.style.pieceGradient, lineWidth: lineWidthForPieceSign)
+            } else {
+                XGradientShape(gradient: viewModel.style.pieceGradient, lineWidth: lineWidthForPieceSign, insetAmount: insetAmuontForPieceSign)
+            }
+        }
+        .zIndex(viewModel.zIndex)
+        .frame(width: size.width, height: size.height)
+        .offset(viewModel.relativeOffset)
+        .gesture(
+            DragGesture(coordinateSpace: .global)
+                .onChanged(viewModel.onDragChanged)
+                .onEnded(viewModel.onDragEnded))
+        .allowsHitTesting(!isDisabled)
+        .overlay(GeometryReader { proxy in
+            Color.clear
+                .onAppear(perform: {
+                    self.viewModel.onAppear(proxy)
+                })
+        })
+    }
+}
+
+#if DEBUG
+
+struct PieceView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PieceView(viewModel: PieceViewModel(with: .X), size: CGSize(width: 80, height: 80))
+            PieceView(viewModel: PieceViewModel(with: .O), size: CGSize(width: 80, height: 80))
+                .colorScheme(.dark)
+        }
+    }
+}
+
+#endif
+
 struct DiagonalLineShape: Shape {
 
     let lineWidth: CGFloat
@@ -83,73 +153,3 @@ enum PieceStyle: String, Codable {
         LinearGradient(colorStart, colorEnd, startPoint: .leading, endPoint: .trailing)
     }
 }
-
-struct PieceView: View {
-    
-    @ObservedObject var viewModel: PieceViewModel
-    var size: CGSize
-
-    var body: some View {
-        let isDragged = viewModel.zIndex == ZIndex.playerPieceDragged
-        let isDisabled = viewModel.disabled
-        let shadowCastedRadius =  viewModel.stateMultiplier * (isDisabled ? 0.0 : size.height / 40.0)
-        let lightSourceShadowRadius = viewModel.stateMultiplier == 1.0 ? (isDisabled ? 0.0 : size.height / 40.0) : 0
-        let blurAmount = 1 * viewModel.stateMultiplier
-        let lineWidthForPieceSign = size.height / 9.0
-        let insetAmuontForPieceSign = size.height / 3.5
-        return ZStack {
-            Circle()
-                .fill(Theme.Col.piece)
-                .shadow(color: Theme.Col.lightSource, radius: lightSourceShadowRadius, x: -lightSourceShadowRadius, y: -lightSourceShadowRadius)
-                .shadow(color: Theme.Col.shadowCasted, radius: shadowCastedRadius, x: shadowCastedRadius, y: shadowCastedRadius)
-                .animation(Animation.default)
-                .blur(radius: blurAmount)
-            Circle()
-                .fill(Theme.Col.piece)
-            if isDragged || isDisabled {
-                Circle()
-                    .fill(LinearGradient(Theme.Col.lightSource, Theme.Col.shadowCasted))
-            }
-            if !isDisabled {
-                Circle()
-                    .stroke(LinearGradient(Theme.Col.lightSource, Theme.Col.shadowCasted), lineWidth: shadowCastedRadius / 2.0)
-                    .blur(radius: shadowCastedRadius / 2.0)
-            }
-            if viewModel.style == .O {
-                Circle()
-                    .inset(by: insetAmuontForPieceSign)
-                    .stroke(viewModel.style.pieceGradient, lineWidth: lineWidthForPieceSign)
-            } else {
-                XGradientShape(gradient: viewModel.style.pieceGradient, lineWidth: lineWidthForPieceSign, insetAmount: insetAmuontForPieceSign)
-            }
-        }
-        .zIndex(viewModel.zIndex)
-        .frame(width: size.width, height: size.height)
-        .offset(viewModel.relativeOffset)
-        .gesture(
-            DragGesture(coordinateSpace: .global)
-                .onChanged(viewModel.onDragChanged)
-                .onEnded(viewModel.onDragEnded))
-        .allowsHitTesting(!isDisabled)
-        .overlay(GeometryReader { proxy in
-            Color.clear
-                .onAppear(perform: {
-                    self.viewModel.onAppear(proxy)
-                })
-        })
-    }
-}
-
-#if DEBUG
-
-struct PieceView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            PieceView(viewModel: PieceViewModel(with: .X), size: CGSize(width: 80, height: 80))
-            PieceView(viewModel: PieceViewModel(with: .O), size: CGSize(width: 80, height: 80))
-                .colorScheme(.dark)
-        }
-    }
-}
-
-#endif

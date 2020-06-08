b816b70bdf1fc90152ee08437eda2cf4a243129c
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-06-08 07:49:02 +0530

    timers: remove delay/gap between timers

--- Tim-Bak-Toe/ViewModels/TimerViewModel.swift
+++ Tim-Bak-Toe/ViewModels/TimerViewModel.swift
@@ -30,11 +30,6 @@ class TimerViewModel: ObservableObject {
     var emptyPublisher = PassthroughSubject<UUID, Never>()
 
     private var cancellables = Set<AnyCancellable>()
-    private var timerObserver: AnyCancellable?
-    
-    private let timerStride = 1.0
-
-    private var timer: Publishers.Autoconnect<Timer.TimerPublisher>?
 
     private var currentState: TimerState = .waiting
 
@@ -43,113 +38,95 @@ class TimerViewModel: ObservableObject {
             guard oldValue != currentTime else { return }
             var ratio = currentTime / GameSettings.maxTurnDuration
             if ratio >= 1 { ratio = 1.0 }
-            if ratio <= 0 { ratio = 0.0}
+            if ratio <= 0 { ratio = 0.0 }
 
             if self.currentState == .waiting {
                 withAnimation {
                     currentFill = CGFloat(ratio)
                 }
             } else {
-                withAnimation(Animation.linear(duration: 1.0)) {
+                withAnimation(Animation.linear(duration: GameSettings.timerStride + 0.1)) {
                     currentFill = CGFloat(ratio)
                 }
             }
         }
     }
 
-    // MARK: - Starting a new game -
-
-    func subscribeToGameStart(_ publisher: PassthroughSubject<UUID, Never>) {
-        publisher.sink { teamId in
-            if self.teamId == teamId {
-                self.startTimer()
-            }
-        }
-        .store(in: &cancellables)
-    }
+//    // MARK: - Starting a new game -
+//
+//    func subscribeToGameStart(_ publisher: PassthroughSubject<UUID, Never>) {
+//        publisher
+//            .filter { self.teamId == $0 }
+//            .sink { _ in self.startTimer() }
+//            .store(in: &cancellables)
+//    }
 
     // MARK: - Successful drop for a team -
     
     func subscribeToNewOccupancy(_ publisher: PassthroughSubject<UUID, Never>) {
-        // instant empty for self
         publisher
-            .filter { $0 == self.teamId }
-            .sink { _ in
-                self.reset() }
-        .store(in: &cancellables)
+            .filter { self.teamId == $0 }
+            .sink { _ in self.reset() }
+            .store(in: &cancellables)
 
-        // instant refill for opponent
         publisher
-            .filter { $0 != self.teamId }
-            .sink { _ in
-                self.startTimer()
-        }
-        .store(in: &cancellables)
+            .filter { self.teamId != $0 }
+            .sink { _ in self.startTimer() }
+            .store(in: &cancellables)
     }
     
     // MARK: - A timer emptied -
 
     func subscribeToEmptiedTimer(_ publisher: PassthroughSubject<UUID, Never>) {
-        publisher.sink { teamId in
-            if self.teamId == teamId {
-                self.reset()
-            } else {
-                self.startTimer()
-            }
-        }
-        .store(in: &cancellables)
+        publisher
+            .filter { self.teamId == $0 }
+            .sink { _ in self.reset() }
+            .store(in: &cancellables)
+
+        publisher
+            .filter { self.teamId != $0 }
+            .sink { _ in self.startTimer() }
+            .store(in: &cancellables)
     }
 
     // MARK: - Any team wins -
 
     func subscribeToWin(_ publisher: PassthroughSubject<UUID, Never>) {
         publisher
-            .delay(for: .seconds(timerStride), scheduler: RunLoop.main)
-            .sink { _ in
-            self.reset()
-        }
-        .store(in: &cancellables)
+//            .delay(for: .seconds(GameSettings.timerStride), scheduler: RunLoop.main)
+            .sink { _ in self.reset() }
+            .store(in: &cancellables)
     }
 
     // MARK: - Game restarts -
 
     func subscribeToRestart(_ publisher: PassthroughSubject<Void, Never>) {
-        publisher.sink { _ in
-            self.reset()
-        }
-        .store(in: &cancellables)
+        publisher
+            .sink { _ in self.reset() }
+            .store(in: &cancellables)
     }
 
-    // MARK: - internal functionalities -
+    // MARK: - Internal functionalities -
 
     private func reset() {
-        timerObserver?.cancel()
-        timer = nil
         currentState = .waiting
         currentTime = GameSettings.maxTurnDuration
     }
     
     private func startTimer() {
         currentState = .emptyingDown
-        timerObserver?.cancel()
-
-        timer = nil
-        timer = Timer.publish(every: timerStride, on: .current, in: .default).autoconnect()
-        startObservingTimer()
-    }
-
-    private func startObservingTimer() {
-        timerObserver = timer?.sink { _ in
-            self.invokeTimerActions()
-        }
+        invokeTimerActions()
     }
 
     private func invokeTimerActions() {
-        currentTime -= timerStride
-        if currentTime <= 0 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + timerStride) {
+        guard self.currentState == .emptyingDown else { return }
+        currentTime -= GameSettings.timerStride
+        DispatchQueue.main.asyncAfter(deadline: .now() + GameSettings.timerStride) { [weak self] in
+            guard let self = self else { return }
+            if self.currentTime <= 0 {
                 self.emptyPublisher.send(self.teamId)
             }
+            self.invokeTimerActions()
         }
     }
 }
